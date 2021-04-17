866e2723957ee9281cf4de39797fe88c36a22364
Jaehoon Han <JaehoonHan1@gmail.com>
2021-04-17 04:50:16 -0400

    create stock models

---
+++ backend/models/stock.model.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Define stock portfolio Schema
+const stockSchema = new Schema({
+    // Attributes and data types
+    username: { 
+        type: Schema.ObjectId, 
+        ref: 'user',
+        required: true 
+    },
+    ticker: { 
+        type: String, 
+        required: true
+    },
+    date: { 
+        type: Date,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        require: true
+    },
+
+}, {
+    // Create timestamp for each entry
+    timestamps: true,
+});
+
+const Stock = mongoose.model('Stock', stockSchema);
+
+module.exports = Stock;
